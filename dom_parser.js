236cf139a03650352f700c91faf314f381f2244f
Martin Akolo Chiteri <martin.chiteri@gmail.com>
2013-09-01 12:43:28 +0300

    Playing around with recursive functions in JavaScript

---
+++ dom_parser.js
@@ -0,0 +1,34 @@
+// Define a walk_the_DOM function that visits every node of the 
+// tree in HTML source order, starting from some given node. It 
+// invokes a function passing it each node in turn. walk_the_DOM 
+// calls itself to process each of the child nodes. 
+
+var DOM_PARSER = {}; 
+
+DOM_PARSER.walk_the_DOM = function walk(node, func) {
+    func(node); 
+    node = node.firstChild; 
+    
+    while(node) { 
+        walk(node, func); 
+        node = node.nextSibling;
+    } // End while   
+};
+
+// Define a getElementsByAttribute function. It takes an attribute 
+// name string and an optional matching value. It calls walk_the_DOM, 
+// passing it a function that looks for an attribute name in the node. 
+// The matching nodes are accumulated in a results array 
+DOM_PARSER.getElementsByAttribute = function (att, value) {
+    var results = [];
+    
+    DOM_PARSER.walk_the_DOM(document.body, function(node) { 
+        var actual = node.nodeType === 1 && node.getAttibute(att); 
+
+        if (typeof actual === 'string' && 
+            (actual === value || typeof value !== 'string' ) )
+            results.push(node);
+    }); // End walk_the_DOM invocation 
+
+    return results; 
+};
